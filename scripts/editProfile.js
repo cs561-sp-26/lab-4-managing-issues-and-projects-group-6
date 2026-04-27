963f38e7cfa8a40b20cf49ddcb348a74e1e7849d
Trisha Gopinath <gopinatt@oregonstate.edu>
2026-04-27 13:39:57 -0700

    #6: Add populateProfileSettingsForm to load user data

--- scripts/editProfile.js
+++ scripts/editProfile.js
@@ -83,3 +83,42 @@
     GlobalsgSettingsBtn.classList.add("collapsed");
     GlobalsgSettingsPanel.classList.remove("show");
 }
+/*************************************************************************
+ * @function populateProfileSettingsForm 
+ * @Desc 
+ * Populates the "Account and Profile Settings" dialog form with the 
+ * current user's data. 
+ * The following global vars are used to access fields in the form
+ *  @global GlobalProfileEmailField
+ *  @global GlobalProfilePasswordField
+ *  @global GlobalProfileSecurityQuestionField
+ *  @global GlobalProfileSecurityAnswerField
+ *  @global GlobalProfileDisplayNameField
+ *  @global GlobalProfilePicImage
+ *  @global GlobalProfileBioField    
+ *  @global GlobalProfileBestStrokesField
+ *  @global GlobalProfileBestMinutesField
+ *  @global GlobalProfileBestSecondsField
+ *  @global GlobalProfileBestCourseField
+ *************************************************************************/
+ function populateProfileSettingsForm() {
+    GlobalProfileEmailField.value = GlobalUserData.accountInfo.email;
+    GlobalProfilePasswordField.value = GlobalUserData.accountInfo.password;
+    GlobalProfileSecurityQuestionField.value = GlobalUserData.accountInfo.securityQuestion;
+    GlobalProfileSecurityAnswerField.value = GlobalUserData.accountInfo.securityAnswer;
+    GlobalProfileDisplayNameField.value = GlobalUserData.identityInfo.displayName;
+    GlobalProfilePicImage.setAttribute("src",GlobalUserData.identityInfo.profilePic);
+    GlobalProfileBioField.value = GlobalUserData.speedgolfInfo.bio;
+    GlobalProfileHomeCourseField.value = GlobalUserData.speedgolfInfo.homeCourse;
+    GlobalProfileFirstRoundField.value = GlobalUserData.speedgolfInfo.firstRound;
+    GlobalProfileBestStrokesField.value = GlobalUserData.speedgolfInfo.personalBest.strokes;
+    GlobalProfileBestMinutesField.value = GlobalUserData.speedgolfInfo.personalBest.minutes;
+    GlobalProfileBestSecondsField.value = GlobalUserData.speedgolfInfo.personalBest.seconds;
+    GlobalProfileBestCourseField.value = GlobalUserData.speedgolfInfo.personalBest.course;   
+    //Check checkboxes...
+    for (const prop in GlobalUserData.speedgolfInfo.clubs) {
+        document.getElementById("sg" + prop).checked = true;
+    }
+    GlobalProfileClubCommentsField.value = GlobalUserData.speedgolfInfo.clubComments;
+    GlobalProfileEmailField.focus(); //Set focus to first field.
+}
