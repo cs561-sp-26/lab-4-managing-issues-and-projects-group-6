a34185bd900d17191c8b1b4e77b4eb84f8d4afd9
kaveenaya <kaveeom@gmail.com>
2026-04-27 21:24:41 -0700

    Updated code to show an empty state when user has no rounds (#29)

--- scripts/roundsMode.js
+++ scripts/roundsMode.js
@@ -124,3 +124,36 @@ GlobalRoundUpdatedMsg.textContent = "New Round Logged!";
 GlobalRoundUpdated.classList.remove("hidden");
 transitionFromDialog(GlobalRoundsModeDialog);
 }
+
+/*************************************************************************
+* @function keyDownRoundDialogFocused 
+* @desc 
+* When the user presses a key with an element in the Log Round 
+* dialog focused, we implement the accessible keyboard interface for
+* a modal dialog box. This means that "Escape" dismisses the dialog and
+* that it is impossible to tab outside of the dialog box.
+* @global GlobalFirstFocusableLogRoundItem: References the first focusable
+*         item in "Log Round" dialog. 
+* @global GlobalRoundsModeLogCancelBtn: The "Cancel" button (last focusable 
+*         item in "Log Round" dialog)
+*************************************************************************/
+function keyDownRoundDialogFocused(e) {
+if (e.code === "Escape") {
+  GlobalRoundsModeLogCancelBtn.click();
+  return;
+}
+if (e.code === "Tab" && document.activeElement == GlobalFirstFocusableLogRoundItem.get() &&
+   e.shiftKey) {
+    //shift focus to last focusable item in dialog
+    GlobalRoundsModeLogCancelBtn.focus();
+    e.preventDefault();
+    return;
+}
+if (e.code === "Tab" && document.activeElement == GlobalRoundsModeLogCancelBtn &&
+    !e.shiftKey) {
+    //shift focus to first focusable item in dialog
+    GlobalFirstFocusableLogRoundItem.get().focus();
+    e.preventDefault();
+    return;
+}
+}
